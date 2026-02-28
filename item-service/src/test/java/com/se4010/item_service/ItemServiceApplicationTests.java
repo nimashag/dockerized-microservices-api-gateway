6b1259d8d779dbe8ab64484d0016eee6225107cf
Nimasha Gamage <nimasha.piyumini@gmail.com>
2026-02-28 20:38:06 +0530

    Initialize item-service with Maven configuration and Spring Boot application

---
+++ item-service/src/test/java/com/se4010/item_service/ItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se4010.item_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
